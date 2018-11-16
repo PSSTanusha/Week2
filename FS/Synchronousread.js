6387a28a0a2fbe77a8072bce97419dd1d539e4ca
PSSTanusha <psstanusha@gmail.com>
2018-11-16 19:14:56 +0530

    Update Synchronousread.js

--- FS/Synchronousread.js
+++ FS/Synchronousread.js
@@ -9,3 +9,39 @@ var data = fs.readFileSync('File1.txt');
 
  var data = fs.readFileSync('File3.txt');
  console.log( data.toString());
+
+output:
+C:\Users\tanus\.vscode\extensions>node sync.js
+Synchronous way
+
+Starting 1st File
+
+File1---file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1---
+---file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1---
+---file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1---
+---file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1---
+---file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1------file1---
+---file1------file1------file1------file1------file1------file1---
+
+
+End of 1st file
+
+Starting 2nd file
+
+---file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2---
+---file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2---
+---file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2---
+---file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2---
+---file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2---
+---file2------file2------file2------file2------file2------file2------file2------file2------file2------file2------file2---
+---file2------file2------file2------file2------file2------file2------file2---
+
+End of 2nd file
+
+Starting 3rd File
+
+---file3------file3------file3------file3------file3------file3------file3------file3------file3------file3------file3------file3------file3---
+---file3------file3------file3------file3------file3------file3------file3------file3------file3------file3------file3------file3------file3---
+---file3------file3------file3---
+
+End of 3rd file
